5b3a542d5ce162751426f232c496a3660bef634c
Hrutik-Naik <hrutiknaik2@gmail.com>
2026-02-19 14:43:36 +0530

    third commit

--- producer/src/components/producer.jsx
+++ producer/src/components/producer.jsx
@@ -1,10 +1,9 @@
 
 import './producer.css'
-import img1 from '../assets/iron.jpg'
-import img2 from '../assets/tobi.jpg'
-import img3 from '../assets/north.jpg'
-import img4 from '../assets/yuva.jpg'
-import img5 from '../assets/kabza.jpg'
+import img1 from '../assets/anathan.jpg'
+import img2 from '../assets/1920.jpg'
+import img3 from '../assets/james.jpg'
+import img4 from '../assets/ong.jpg'
 import img6 from '../assets/user.png'
 
 function Prod() {
@@ -28,79 +27,61 @@ function Prod() {
         </div>
         <div className='bottom'>
           <div className="b-t">
-          <div className='bh'>PENDING MOVIES</div>
+          <div className='bh'>APPROVED MOVIES</div>
           <div className="bh1"></div>
           </div>
           <div className="b-b">
           <div className="b-l">
           <div className="l1">
           <div className="b1">
-            <img src={img1} alt="" className='iron' />
+            <img src={img1} alt="" className='anathan' />
           </div>
           <div className="b11">
-            <div className="bb1"><b>Ironman 3</b></div>
+            <div className="bb1"><b>Anathan kaadu</b></div>
             <div className="bb2"></div>
-            <div className="bb3"><h5>English</h5></div>
-            <div className="bb4"><h5>03/05/2013</h5></div>
+            <div className="bb3"><p>English</p></div>
+            <div className="bb4"><p>22/04/2022</p></div>
           </div>
           </div>
           <div className="l2">
           <div className="b2">
-            <img src={img2} alt="" className='tobi' />
+            <img src={img2} alt="" className='a1920' />
           </div>
           <div className="b22">
-            <div className="bb5"><b>Tobi</b></div>
+            <div className="bb5"><b>1920</b></div>
             <div className="bb6"></div>
-            <div className="bb7"><h5>English</h5></div>
-            <div className="bb8"><h5>22/04/2026</h5></div>
-          </div>
-          </div>
-          <div className="l3">
-          <div className="b3">
-            <img src={img3} alt="" className='north'/>
-          </div>
-          <div className="b33">
-            <div className="bb9"><b>Northmen</b></div>
-            <div className="bb10"></div>
-            <div className="bb11"><h5>Englidh</h5></div>
-            <div className="bb12"><h5>22/04/2024</h5></div>
+            <div className="bb7"><p>English</p></div>
+            <div className="bb8"><p>22/04/2026</p></div>
           </div>
           </div>
           </div>
           <div className="b-r">
             <div className="r1">
             <div className="b4">
-              <img src={img3} alt=""className='north' />
+              <img src={img3} alt=""className='james' />
             </div>
             <div className="b44">
-               <div className="bb9"><b>Northmen</b></div>
+               <div className="bb9"><b>James</b></div>
             <div className="bb10"></div>
-            <div className="bb11"><h5>Englidh</h5></div>
-            <div className="bb12"><h5>22/04/2024</h5></div>
+            <div className="bb11"><p>Englidh</p></div>
+            <div className="bb12"><p>03/05/2026</p></div>
             </div>
             </div>
             <div className="r2">
             <div className="b5">
-              <img src={img4} alt="" className='yuva'/>
+              <img src={img4} alt="" className='ong'/>
             </div>
              <div className="b55">
-              <div className="bb13"><b>Yuva</b></div>
+              <div className="cl">
+              <div className="bb13"><b>Ong bac</b></div>
               <div className="bb14"></div>
-              <div className="bb15"><h5>English</h5></div>
-              <div className="bb16"><h5>22/04/2022</h5></div>
+              <div className="bb15"><p>English</p></div>
+              <div className="bb16"><p>22/04/2022</p></div>
+              </div>
+              <div className="cr">
+                <button className='btn'>+</button>
+              </div>
              </div>
-            </div>
-            <div className="r3">
-            <div className="b6">
-              <img src={img5} alt="" className='kabza'/>
-            </div>
-            <div className="b66">
-              <div className="bb17"><b>Kabza</b></div>
-              <div className="bb18"></div>
-              <div className="bb19"><h5>English</h5></div>
-              <div className="bb20"><h5>22/04/</h5></div>
-            </div>
-
             </div>
             </div>
           </div>
