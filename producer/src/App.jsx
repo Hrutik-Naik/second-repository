f53857dda91ee8ae0adaea06b2114ad6921fbc77
Hrutik-Naik <hrutiknaik2@gmail.com>
2026-02-25 14:19:52 +0530

    ninth commit

--- producer/src/App.jsx
+++ producer/src/App.jsx
@@ -2,8 +2,8 @@
 //  import Hrutik from './components/hrutik'
 // import Hrutik1 from './components/hrutik1'
 //  import Hrutik2 from './components/hrutik2'
-//  import Hrutik3 from './components/hrutik3'
-import Me from './components/me'
+ import Hrutik3 from './components/hrutik3'
+// import Me from './components/me'
 
 function App() {
 
@@ -12,12 +12,11 @@ function App() {
     <div className="app">
      
     </div>
-      {/* <Prod/>   */}
        {/* <Hrutik/>  */}
         {/* <Hrutik1/>   */}
          {/* <Hrutik2/>  */}
-       {/* <Hrutik3/>  */}
-       <Me/>
+       <Hrutik3/> 
+       {/* <Me/> */}
     </>
     
   )
--- producer/src/components/hrutik3.jsx
+++ producer/src/components/hrutik3.jsx
@@ -1,33 +1,51 @@
 import './hrutik3.css'
-import img1 from '../assets/left.png'
+import img1 from '../assets/left1.png'
 import img2 from '../assets/user1.png'
 import img3 from '../assets/door.png'
+import img4 from '../assets/more.png'
 function Hrutik3() {
     return(
         <>
         <div className="main">
-         <div className="top">
-            <div className="t1">
-                <img src={img1} alt="" className='left' />
+        <div className="top">
+            <button className="left">
+                <img src={img1} alt=""className="arrow1" />
+            </button>
+            <div className="right"><h3>Profile</h3></div>
+        </div>
+        <div className="bottom">
+            <div className="card">
+                <div className="top1">
+                    <div className="top2">
+                        <div className="circle">AD
+                        </div>
+                            <div className="circle1"></div>
+                    </div>
+                    <div className="middle2"><h3>Producer</h3></div>
+                    <div className="bottom2"><p>producer@test.com</p></div>
+                </div>
+                <div className="middle1">
+                    <div className="left3">
+                        <div className="left4">
+                            <img src={img2} alt=""className="profile" />
+                        </div>
+                        <div className="right4"><h4>Edit Profile</h4></div>
+                    </div>
+                    <div className="right3">
+                        <div className="arrow">
+                            <img src={img4} alt="" />
+                        </div>
+                    </div>
+                    </div>
+                <div className="bottom1">
+                    <div className="left5">
+                        <img src={img3} alt="" className="door"/>
+                    </div>
+                    <div className="right5"><h4>Logout</h4></div>
+                </div>
             </div>
-            <div className="t2">Profile</div>
-         </div>
-         <div className="middle">
-            <div className="m1">PR</div>
-            <div className="m2">
-                <div className="m11">Producer</div>
-                <div className="m12">producer@test.com</div>
-            </div>
-         </div>
-         <div className="bottom">
-                <button className="b1">
-                <img src={img2} alt="" className='user' />Edit Profile</button>
-                <button className="b2">
-                    <img src={img3} alt="" className='door' />Logout
-                </button>
-         </div>
-         </div>
-
+        </div>
+        </div>
 
         </>
     )
